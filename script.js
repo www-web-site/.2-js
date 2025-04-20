797bcd11279336c109f581eb6e90b2d184a39ab0
www-web-site <329blogertop228@gmail.com>
2025-04-20 10:58:34 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const string = prompt('String typedagi malumotni kiriting:');
+const number = prompt('Number typedagi malumotni kiriting:');
+const boolean = prompt('Boolean typedagi malumotni kiriting:');
+const object = confirm('Hamma malumot togrimi?');
+alert(`String: ${string}\ Number: ${number}\ Boolean: ${boolean}`);
